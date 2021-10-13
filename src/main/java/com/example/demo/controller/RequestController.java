4454f20d3fb15a1b25514f1ed366b3bc0b7fde7b
Raj <nithinithya46@gmail.com>
2021-10-13 10:37:29 +0530

    update

--- src/main/java/com/example/demo/controller/RequestController.java
+++ src/main/java/com/example/demo/controller/RequestController.java
@@ -1,12 +1,16 @@
 package com.example.demo.controller;
 
+import java.util.ArrayList;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 import com.example.demo.modal.Request;
 import com.example.demo.repository.RequestRepository;
 
@@ -25,5 +29,12 @@ public class RequestController {
 				.body("Request sent successfully!");
 		
 	}	
+	@GetMapping(value = "/getAll")
+	public ResponseEntity<?> getAllRequests(){
+		ArrayList<Request> requests = (ArrayList<Request>) RequestRepo.findAll();
+		return ResponseEntity
+				.status(HttpStatus.OK)
+				.body(requests);
+	}
 	
 }
